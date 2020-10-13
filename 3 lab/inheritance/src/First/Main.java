83d58ae1436d190b008896504ea88eba4bd5abaf
VanDerLindeDutch <sotn.daniil@yandex.ru>
2020-10-13 13:02:46 +0300

    FinishedConverter

--- 3 lab/inheritance/src/First/Main.java
+++ 3 lab/inheritance/src/First/Main.java
@@ -4,15 +4,18 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Shape shape = new Shape(2.4);
-        Pyramid pyramid = new Pyramid(1.4,2.3,4.5);
-        Cylinder cylinder = new Cylinder(1.2,34.5,3.1,2.9);
-        Cylinder cylinder1 = new Cylinder(2.2,34.5,3.1,2.9);
-        Box box = new Box(3);
-        box.add(shape);
-        box.add(pyramid);
-        box.add(cylinder);
-        box.add(cylinder1);
-        System.out.println(box);
+//        Shape shape = new Shape(2.4);
+//        Pyramid pyramid = new Pyramid(1.4,2.3,4.5);
+//        Cylinder cylinder = new Cylinder(1.2,34.5,3.1,2.9);
+//        Cylinder cylinder1 = new Cylinder(2.2,34.5,3.1,2.9);
+//        Box box = new Box(3);
+//        box.add(shape);
+//        box.add(pyramid);
+//        box.add(cylinder);
+//        box.add(cylinder1);
+        SolidOfRevolution SOR = new SolidOfRevolution(2, 1, (x) -> x*x-16);
+        System.out.println(SOR.calculateVolume());
+
+
     }
 }
